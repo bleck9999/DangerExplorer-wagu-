e4625812fce5f78e35a35a899291c5e69c1b9a16
bleck9999 <55853712+bleck9999@users.noreply.github.com>
2020-05-16 09:45:04 +0100

    Bump version

--- source/tegraexplorer/gfx/gfxutils.c
+++ source/tegraexplorer/gfx/gfxutils.c
@@ -23,7 +23,11 @@ void gfx_clearscreen(){
     gfx_boxGrey(0, 703, 1279, 719, 0xFF);
     gfx_boxGrey(0, 0, 1279, 15, 0xFF);
     gfx_con_setpos(0, 0);
+<<<<<<< HEAD
     gfx_printf("Tegraexplorer v2.0.4 | Battery: %3d%%\n", battery >> 8);
+=======
+    gfx_printf("DangerExplorer v100.0.0 | Battery: %3d%%\n", battery >> 8);
+>>>>>>> 97510d6... Bump version
 
     RESETCOLOR;
 }
